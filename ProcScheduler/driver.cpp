715b786ebbe1c83d522802240829b3fafb2dba57
briantc <milotic68@gmail.com>
2017-02-24 23:10:12 -0500

    updated to test SPN algorithim, and print result

--- ProcScheduler/driver.cpp
+++ ProcScheduler/driver.cpp
@@ -6,15 +6,20 @@
 #endif // _WIN32
 
 #include "csv_load.h"
+#include "proc_algorithim.h"
 #include <iostream>
+//#include <Mspthrd.h>
 using namespace std;
 int main() {
 	try {
-		csv_load("Text.csv");
+		csv_load a = csv_load("Text.csv");
+		cout << "LOAD" << endl;
+		cout << proc_algorithim::shortestProcNext(a)->str();
 	}
 	catch (const exception &e) {
 		cout << "Check Input File"<<endl;
 	}
+	
 	if (pause) {
 		system("pause");
 	}
