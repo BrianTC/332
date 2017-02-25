728e806f5f7c6f32e658b76474a5a1f3fdb8f835
briantc <milotic68@gmail.com>
2017-02-24 23:11:28 -0500

    Completed SPN algorithim emulation modified return types of functions to ostringstream* from void

--- ProcScheduler/proc_algorithim.cpp
+++ ProcScheduler/proc_algorithim.cpp
@@ -1,14 +1,62 @@
 #include "proc_algorithim.h"
-void proc_algorithim::roundRobin(csv_load) {
+ostringstream* proc_algorithim::shortestProcNext(csv_load a) {
+	ostringstream *printOut=new ostringstream();
+	*printOut << "ShortestProcessNext:\n";
+	vector<csv_load::proc> procList = a.csv_getProcList();
+	int clock = 0;
+	vector<csv_load::proc> cProcList;
+	while (procList.size() > 0 || cProcList.size() > 0) {
+		//Queue processes ready to be run
+		for (size_t i = 0; i < procList.size(); i++) {
+			if (procList[i].t_start <= clock){
+				cProcList.push_back(procList[i]);
+				procList.erase(procList.begin() + i);
+				i--;
+			}
+		}
+		//cout << cProcList.size() << endl;
+		//Process Shortest Process Next
+		csv_load::proc shortestProcess;
+		int shortestProcessLen,shortIndex;
+		for (size_t i = 0; i < cProcList.size(); i++) {
+			//initialize first value 
+			if (i == 0){
+				shortestProcess = cProcList[0];
+				shortIndex = 0;
+				shortestProcessLen = shortestProcess.t_run;
+			}
+			//find next shortest value
+			if (cProcList[i].t_run < shortestProcessLen){
+				shortestProcessLen = cProcList[i].t_run;
+				shortestProcess = cProcList[i];
+				shortIndex = i;
+			}
+		}
+		if (cProcList.size()>0) {
+			//std::cout << shortestProcess.pid;
+			cProcList.erase(cProcList.begin() + shortIndex);
+			for (int i = 0; i < shortestProcessLen; i++) {
+				*printOut << "Cycle: " << setw(IOWIDTH) << clock << " PID: " << shortestProcess.pid << endl;
+				clock++;
+			}
+		}
+		else {
+			*printOut << "Cycle: "  << setw(IOWIDTH) << clock << " EMPTY CYCLE" << endl;
+			clock++;
+		}
+
+	}//END WHILE
+	return printOut;
 	//TODO;
 	//roundRobinAlgorithim
 }
-void proc_algorithim::HRRN(csv_load) {
+
+ostringstream* proc_algorithim::HRRN(csv_load a) {
 
 }
-void proc_algorithim::roundRobin(csv_load) {
+ostringstream* proc_algorithim::roundRobin(csv_load a) {
 
 }
-void proc_algorithim::shortestProcNext(csv_load) {
+ostringstream* proc_algorithim::feedback(csv_load a) {
 
 }
--- ProcScheduler/proc_algorithim.h
+++ ProcScheduler/proc_algorithim.h
@@ -1,11 +1,15 @@
 #pragma once
 #include "csv_load.h"
-class proc_algorithim
-{
-public:
-	static void roundRobin(csv_load);
-	static void HRRN(csv_load);
-	static void roundRobin(csv_load);
-	static void shortestProcNext(csv_load);
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+class proc_algorithim{
+	private:
+		static const int IOWIDTH = 4;
+	public:
+		static ostringstream* roundRobin(csv_load a);
+		static ostringstream* HRRN(csv_load a);//Highest response ratio next
+		static ostringstream* feedback(csv_load a);
+		static ostringstream* shortestProcNext(csv_load a);
 };
 
